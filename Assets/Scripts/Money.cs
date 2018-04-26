07ea28a9b2d6bcbc8d2ca81178c93c71a7ecc32a
TyR4 <ty.r.dave@gmail.com>
2018-04-26 14:02:10 -0600

    Added class to track player's money

---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+public class Money
+{
+    public double Amount { get; private set; }
+
+    public Money(int amount)
+    {
+        Amount = amount;
+    }
+
+    public Money() : this(0)
+    {
+    }
+
+    public void Increase(int amount)
+    {
+        Amount += amount;
+    }
+
+    public void Decrease(int amount)
+    {
+        Amount -= amount;
+        if (Amount < 0)
+            Amount = 0;
+    }
+
+    public override string ToString()
+    {
+        return Amount + " LIVRES";
+    }
+
+    public override bool Equals(object obj)
+    {
+        var money = obj as Money;
+        return money != null &&
+               Amount == money.Amount;
+    }
+
+    public override int GetHashCode()
+    {
+        return -602769199 + Amount.GetHashCode();
+    }
+
+    public static bool operator ==(Money money1, Money money2)
+    {
+        return EqualityComparer<Money>.Default.Equals(money1, money2);
+    }
+
+    public static bool operator !=(Money money1, Money money2)
+    {
+        return !(money1 == money2);
+    }
+}
---
+++ Assets/Tests/Editor/MoneyTest.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+public class MoneyTest
+{
+
+    Money money;
+
+    [SetUp]
+    public void BeforeEveryTest()
+    {
+        this.money = new Money(10);
+    }
+
+    [Test]
+    public void Test_MoneyHasAmount()
+    {
+        Assert.AreEqual(10, this.money.Amount);
+    }
+
+    [Test]
+    public void Test_IncreaseAmount()
+    {
+        Assert.AreEqual(10, this.money.Amount);
+        this.money.Increase(1);
+        Assert.AreEqual(11, this.money.Amount);
+    }
+
+    [Test]
+    public void Test_MoneyCanHaveInitialAmountOf0()
+    {
+        Money money = new Money();
+        Assert.AreEqual(0, money.Amount);
+    }
+
+    [Test]
+    public void Test_DecreaseAmount()
+    {
+        Assert.AreEqual(money.Amount, 10);
+        money.Decrease(1);
+        Assert.AreEqual(9, money.Amount);
+    }
+
+    [Test]
+    public void Test_AmountCannotBeNegative()
+    {
+        this.money.Decrease(100);
+        Assert.AreEqual(0, money.Amount);
+    }
+
+    [Test]
+    public void Test_MoneyStringDisplaysLivre()
+    {
+        Assert.AreEqual("10 LIVRES", money.ToString());
+    }
+
+    [Test]
+    public void Test_MoniesEqualIfHaveSameAmount()
+    {
+        Money sameMoney = new Money(10);
+        Assert.AreEqual(money, sameMoney);
+    }
+}

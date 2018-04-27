d615263d57285d6bcff3919f5bce50146c1b513e
TyR4 <ty.r.dave@gmail.com>
2018-04-26 18:06:19 -0600

    added paper to track stock

---
+++ Assets/Scripts/Paper.cs
@@ -0,0 +1,26 @@
+﻿public class Paper : AmountTracker
+{
+
+    public Paper(int amount) : base(amount)
+    {
+    }
+
+    public Paper()
+    {
+    }
+
+    public void Use(int numberOfSheets)
+    {
+        Decrease(numberOfSheets);
+    }
+
+    public void Gain(int numberOfSheets)
+    {
+        Increase(numberOfSheets);
+    }
+
+    public override string ToString()
+    {
+        return Amount + " sheets";
+    }
+}
---
+++ Assets/Tests/Editor/PaperTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+public class PaperTests
+{
+
+    private Paper paper;
+
+    [SetUp]
+    public void BeforeEachTest()
+    {
+        paper = new Paper(10);
+    }
+
+    [Test]
+    public void Test_PaperHasAmount()
+    {
+        Assert.AreEqual(10, paper.Amount);
+    }
+
+    [Test]
+    public void Test_CanUsePaper()
+    {
+        paper.Use(5);
+        Assert.AreEqual(5, paper.Amount);
+    }
+
+    [Test]
+    public void Test_CanGainPaper()
+    {
+        paper.Gain(5);
+        Assert.AreEqual(15, paper.Amount);
+    }
+
+    [Test]
+    public void Test_PaperStringDisplaysSheets()
+    { 
+        Assert.AreEqual("10 sheets", paper.ToString());
+    }
+
+}

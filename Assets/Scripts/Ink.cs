97da783fa3e81d189d04a18e63400734abc2509b
TyR4 <ty.r.dave@gmail.com>
2018-04-27 12:53:31 -0600

    added ink stock

---
+++ Assets/Scripts/Ink.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Ink : AmountTracker
+{
+    public Ink(float amount) : base(amount)
+    {
+        this.UOM = "units";
+    }
+
+    public Ink()
+    {
+        this.UOM = "units";
+    }
+
+    public void Deplete(float amount)
+    {
+        Decrease(amount);
+    }
+
+    public void Add(float amount)
+    {
+        Increase(amount);
+    }
+}
---
+++ Assets/Tests/Editor/InkTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+public class InkTests
+{
+
+    private Ink ink = new Ink(10);
+
+    [SetUp]
+    public void BeforeEachTest()
+    {
+        ink = new Ink(10);
+    }
+
+    [Test]
+    public void Test_InkHasAmount()
+    {
+        Assert.AreEqual(10, ink.Amount);
+    }
+
+    [Test]
+    public void Test_CanAddInk()
+    {
+        ink.Add(4);
+        Assert.AreEqual(14, ink.Amount);
+    }
+
+    [Test]
+    public void Test_CanDepleteInk()
+    {
+        ink.Deplete(4);
+        Assert.AreEqual(6, ink.Amount);
+    }
+
+    [Test]
+    public void Test_InkStringDisplaysUnits()
+    {
+        Assert.AreEqual("10 units", ink.ToString());
+    }
+
+}

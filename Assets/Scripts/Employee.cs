00ddef8e1e350af9c2ffb3070c91a0009c6872f1
TyR4 <ty.r.dave@gmail.com>
2018-04-27 17:48:36 -0600

    beginning work on using employees

---
+++ Assets/Scripts/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Employee
+{
+
+    public double Wage { get; private set; }
+    public int SkillLevel { get; private set; }
+
+    public Employee(int startingWage, int skillLevel)
+    {
+        if (startingWage < 0)
+            throw new ArgumentOutOfRangeException();
+
+        this.Wage = startingWage;
+        this.SkillLevel = skillLevel;
+    }
+
+}
---
+++ Assets/Tests/Editor/EmployeeTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+
+public class EmployeeTests
+{
+
+    [Test]
+    public void Test_EmployeesHaveStartingWages()
+    {
+        int startingWage = 10;
+        Employee employee = new Employee(startingWage, 0);
+        Assert.AreEqual(startingWage, employee.Wage);
+    }
+
+    [Test]
+    public void Test_EmployeesCannotHaveNegativeWage()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Employee(-10, 0));
+    }
+
+    [Test]
+    public void Test_EmployeesHaveSkillLevels()
+    {
+        Employee employee = new Employee(10, 1);
+        Assert.AreEqual(1, employee.SkillLevel);
+    }
+
+}
